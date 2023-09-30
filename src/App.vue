7a43a8639350f4a8bb9734e724957c8a094c90ca
PietroCipolla95 <pietro.cipolla.95@gmail.com>
2023-09-30 15:27:21 +0200

    add 2 list for movies and series

--- src/App.vue
+++ src/App.vue
@@ -9,15 +9,16 @@ export default {
       state,
       query: '',
       movie: '',
+      serie: '',
     }
   },
 
   methods: {
 
-    showMovies(query) {
+    showTvMovie(query) {
 
       query = state.query
-      state.fetchMovies(query)
+      state.fetchDatas(query)
       console.log('stai cercando..', query);
 
     },
@@ -30,26 +31,54 @@ export default {
 
 <template>
   <input type="search" v-model="state.query">
-  <button @click="showMovies">cerca</button>
+  <button @click="showTvMovie">cerca</button>
 
-  <ul>
-    <li v-for="movie in  state.movies ">
-      <p>
-        Title: {{ movie.original_title }}
-      </p>
-      <p>
-        OG Title: {{ movie.title }}
-      </p>
-      <p>
-        Lang: <img
-          :src="movie.original_language = 'en' ? 'https://flagsapi.com/GB/flat/64.png' : 'https://flagsapi.com/' + movie.original_language.toUpperCase() + '/flat/64.png'"
-          alt="" :width="40">
-      </p>
-      <p>
-        Avg Score: {{ movie.vote_average }}
-      </p>
-    </li>
-  </ul>
+
+  <div class="d-flex">
+
+    <ul>
+      <h2 v-if="state.movies.length > 0">Films</h2>
+      <li v-for="movie in  state.movies">
+        <p>
+          Title: {{ movie.original_title }}
+        </p>
+        <p>
+          OG Title: {{ movie.title }}
+        </p>
+        <p>
+          Lang: <img
+            :src="movie.original_language = 'en' ? 'https://flagsapi.com/GB/flat/64.png' : 'https://flagsapi.com/' + movie.original_language.toUpperCase() + '/flat/64.png'"
+            alt="" :width="40">
+        </p>
+        <p>
+          Avg Score: {{ movie.vote_average }}
+        </p>
+      </li>
+    </ul>
+
+    <ul>
+      <h2 v-if="state.series.length > 0">Tv Series</h2>
+      <li v-for="serie in state.series">
+        <p>
+          Title: {{ serie.name }}
+        </p>
+        <p>
+          Og Title: {{ serie.original_name }}
+        </p>
+        <p>
+          Lang: <img
+            :src="serie.original_language = 'en' ? 'https://flagsapi.com/GB/flat/64.png' : 'https://flagsapi.com/' + serie.original_language.toUpperCase() + '/flat/64.png'"
+            alt="" :width="40">
+        </p>
+        <p>
+          Avg Score: {{ serie.vote_average }}
+        </p>
+
+      </li>
+    </ul>
+
+
+  </div>
 </template>
 
 <style scoped></style>
--- src/state.js
+++ src/state.js
@@ -5,12 +5,16 @@ export const state = reactive({
 
     my_api_key: '41f0b1bdb7855d59b45d0dc81fd4906f',
     movie_url: '',
+    series_url: '',
     query: '',
     movies: [],
+    series: [],
 
-    fetchMovies(query) {
+    fetchDatas(query) {
 
         this.movie_url = 'https://api.themoviedb.org/3/search/movie';
+
+        this.series_url = 'https://api.themoviedb.org/3/search/tv'
 
         axios.get(this.movie_url, {
             params: {
@@ -28,14 +32,27 @@ export const state = reactive({
 
             .catch(error => {
                 console.log(error);
+            }),
+
+            axios.get(this.series_url, {
+                params: {
+                    api_key: this.my_api_key,
+                    query: state.query,
+                }
             })
 
+                .then(response => {
+
+                    this.series = response.data.results
+                    console.log(this.series);
+
+                })
+
+                .catch(error => {
+                    console.log(error);
+                })
+
     }
-
-
-
-
-
 
 
 })
